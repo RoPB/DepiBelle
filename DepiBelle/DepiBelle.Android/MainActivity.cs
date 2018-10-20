dea4052c9702357ef80671823b8620cafa0b110a
Rodrigo Pintos <rodrigo.pintos@uruit.com>
2018-10-19 21:24:19 -0300

    doing some changes

--- DepiBelle/DepiBelle.Android/MainActivity.cs
+++ DepiBelle/DepiBelle.Android/MainActivity.cs
@@ -37,16 +37,19 @@ namespace DepiBelle.Droid
             DependencyContainer.RegisterDependencies();
         }
 
+
         public override void OnBackPressed()
         {
-            if (Rg.Plugins.Popup.Popup.SendBackPressed(base.OnBackPressed))
+            //I disable hardware back button
+
+            /*if (Rg.Plugins.Popup.Popup.SendBackPressed(base.OnBackPressed))
             {
                 // Do something if there are some pages in the `PopupStack`
             }
             else
             {
                 // Do something if there are not any pages in the `PopupStack`
-            }
+            }*/
         }
     }
 }
