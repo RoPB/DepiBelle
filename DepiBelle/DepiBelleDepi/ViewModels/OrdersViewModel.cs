c4135b08776dd21c44aa1cfef38d32786ab89993
Rodrigo Pintos <rodrigo.pintos@uruit.com>
2018-10-22 18:22:28 -0300

    por cargar orders!

--- DepiBelle/DepiBelleDepi/ViewModels/OrdersViewModel.cs
+++ DepiBelle/DepiBelleDepi/ViewModels/OrdersViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using DepiBelleDepi.Managers.Application;
 using DepiBelleDepi.Models;
@@ -9,11 +11,13 @@ using DepiBelleDepi.Utilities;
 
 namespace DepiBelleDepi.ViewModels
 {
-    public class OrdersViewModel:ViewModelBase
+    public class OrdersViewModel : ViewModelBase
     {
         private IConfigService _configService;
         private IApplicationManager _applicationMananger;
         private IDataCollectionService<Order> _ordersDataService;
+
+        private Dictionary<string, Order> _dicOrders = new Dictionary<string, Order>();
 
         public OrdersViewModel()
         {
@@ -21,7 +25,6 @@ namespace DepiBelleDepi.ViewModels
             _configService = _configService ?? DependencyContainer.Resolve<IConfigService>();
             _applicationMananger = _applicationMananger ?? DependencyContainer.Resolve<IApplicationManager>();
             _ordersDataService = _ordersDataService ?? DependencyContainer.Resolve<IDataCollectionService<Order>>();
-
 
         }
 
@@ -33,11 +36,52 @@ namespace DepiBelleDepi.ViewModels
             _ordersDataService.Initialize(new DataServiceConfig() { Uri = _configService.Uri, Key = $"{_configService.Orders}/{date}" });
             await _ordersDataService.Subscribe(async (obj) =>
             {
-
+                await OnOrderCollectionModified(obj);
             });
             var orders = await _ordersDataService.GetAll();
 
+            await LoadOrders(orders);
+
             IsLoading = false;
+        }
+
+        private async Task LoadOrders(List<Order> orders)
+        {
+            orders = orders.OrderBy(o => o.Number).ToList();
+
+            orders.ForEach(o => _dicOrders.Add(o.Id, o));
+        }
+
+        private async Task OnOrderCollectionModified(ServiceSubscriberEventParam<Order> param)
+        {
+            var order = param.Item;
+
+            if (param.Type.Equals(SubscriptionEventType.InsertOrUpdate))
+            {
+
+                await AddUpdateOrder(order);
+            }
+            else
+                await RemoveOrder(order.Id);
+        }
+
+        private async Task AddUpdateOrder(Order order)
+        {
+            //ADD
+            if (!_dicOrders.ContainsKey(order.Id)) 
+            {
+                _dicOrders.Add(order.Id, order);
+            }
+            //UPDATE
+            else
+            {
+                _dicOrders[order.Id] = order;
+            }
+        }
+
+        private async Task RemoveOrder(string id)
+        {
+            _dicOrders.Remove(id);
         }
     }
 }
