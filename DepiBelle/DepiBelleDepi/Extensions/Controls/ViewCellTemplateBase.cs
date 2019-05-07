78edcd1f8d319059bc69492b14bd23a1941dd046
Rodrigo Pintos <rodrigo.pintos@uruit.com>
2019-05-07 14:52:28 -0300

    a little change

---
+++ DepiBelle/DepiBelleDepi/Extensions/Controls/ViewCellTemplateBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace DepiBelleDepi.Extensions
+{
+    public class ViewCellTemplateBase:ViewCell
+    {
+        public static BindableProperty ParentBindingContextProperty = BindableProperty.Create(nameof(ParentBindingContext), typeof(object), typeof(ViewCellTemplateBase), null);
+
+        public object ParentBindingContext
+        {
+            get { return GetValue(ParentBindingContextProperty); }
+            set { SetValue(ParentBindingContextProperty, value); }
+        }
+    }
+}
