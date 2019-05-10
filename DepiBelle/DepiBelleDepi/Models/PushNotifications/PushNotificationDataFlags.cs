353014152d032666d88d11fe0f99a2cc9d9197ef
Rodrigo Pintos <rodrigo.pintos@uruit.com>
2019-05-10 18:46:27 -0300

    ahora anda, debe haber sido el dispositivo

--- DepiBelle/DepiBelleDepi/Models/PushNotifications/PushNotificationDataFlags.cs
+++ DepiBelle/DepiBelleDepi/Models/PushNotifications/PushNotificationDataFlags.cs
@@ -3,7 +3,7 @@ namespace DepiBelleDepi.Models.PushNotifications
 {
     public class PushNotificationDataFlags
     {
-        public static readonly string Flag = "momentum_health";
+        public static readonly string Flag = "depibelle_depi";
         public static readonly string Type = "type";
         public static readonly string Badge = "badge";
         public static readonly string Title = "title";
